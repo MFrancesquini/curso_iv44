0417fb6206de924b021fdb553009c5b94d54fb0f
Marcos <francesquini.professional@gmail.com>
2019-05-25 15:35:26 +0100

    area44_course_odin_challenge_solved

---
+++ funcao.rb
@@ -0,0 +1,57 @@
+def caesar_cipher(phrase, cripto)
+    
+    alfa = [*'a'..'z']
+    key=0    
+    size = phrase.length
+    cripto_phrase=[]
+
+    while size > 0
+       up=false
+       
+       if  phrase[key]==phrase[key].upcase
+           get_letter = phrase[key].downcase
+           up=true
+       else 
+           get_letter = phrase[key]
+       end    
+       key_key=0
+
+       alfa.each do |item|
+             
+           #  if key_key+cripto>alfa.length
+           #     key_key=0
+           #  end
+
+             if item==get_letter
+
+                if up==true
+                   cripto_phrase << alfa[key_key+cripto].upcase
+                else
+                   cripto_phrase << alfa[key_key+cripto]
+                end
+
+                key_key=0
+                break  
+                
+               else
+                
+                cripto_phrase << get_letter
+                break
+
+             end  
+
+             key_key=key_key+1
+
+       end     
+       key=key+1
+       size=size-1
+       
+       
+    end
+
+    print cripto_phrase
+
+
+end
+
+caesar_cipher('Aa#334aaBc!',2) 
